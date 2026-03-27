7e28def41137b8754e07db68bec10f88743d7615
Naman <nayalaunda076@gmail.com>
2026-03-27 23:30:08 +0530

    Small changes

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 
 // ================= MIDDLEWARE =================
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());   // 🔥 allow everything (for now)
 
 app.use(cookieParser());
 app.use(express.json());
